f48f22f930a8c8e6462ca65d47c51d213264cb0c
Jonnii Ringo <jonathansamuels@Jonathans-MBP-2.lan>
2024-12-24 10:24:21 -0800

    Fixed the styling of the h1 element and completed the challenge. Button now works and the set interval now works and dynamically renders

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,44 +1,27 @@
 import React, {useState} from 'react';
-import React from "react"; 
-
-let time = new Date().toLocaleTimeString();
-
-
-
-
 
 function App() {
 
-  const  [time, Date] = useState(0); 
+  const date = new Date().toLocaleTimeString();
+
+  const  [time, setTime] = useState(date); 
 
   function getTime(){
-
-
+    const newDate = new Date().toLocaleTimeString(); 
+    setTime(newDate); 
   };
 
-  function secondTime(){
-    Date( setInterval(time, 1000) ); 
-  };
+ 
+  setInterval(getTime, 8000);
 
   return (
     <div className="container">
       <h1>{time}</h1>
-      <button onClick = {"function"}> {time} </button> 
+      <button onClick = {getTime}>Get Time</button> 
     </div>
   );
 }
 
 
-
-//Challenge:
-//1. Given that you can get the current time using:
-
-
-//Show the latest time in the <h1> when the Get Time button
-//is pressed.
-
-//2. Given that you can get code to be called every second
-//using the setInterval method.
-//Can you get the time in your <h1> to update every second?
 
 export default App;
